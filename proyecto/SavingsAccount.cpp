c3cf860c9a6179e500546b012f53b9f49809df8a
Miranda Martínez <mirandamartinezjimenez8@gmail.com>
2024-05-29 16:18:34 -0600

    Savings

--- proyecto/SavingsAccount.cpp
+++ proyecto/SavingsAccount.cpp
@@ -12,21 +12,23 @@ SavingsAccount::SavingsAccount(std::string account_number, std::string account_h
     : BankAccount(account_number, account_holder_name, balance), _interest_rate(interest_rate) {}
 
 void SavingsAccount::deposit(double amount) {
-    if (amount <= 0) {
-        throw std::invalid_argument("El monto del depósito no puede ser negativo.");
+    if (amount < 0) {
+        throw std::invalid_argument("Error: El monto del depósito no puede ser negativo.");
+    } else if (amount == 0) {
+        throw std::invalid_argument("Error: El monto del depósito no puede ser cero.");
     }
-    BankAccount::deposit(amount);
-    _balance += _balance * (_interest_rate / 100);
+    BankAccount::deposit(amount);  
+    _balance += _balance * (_interest_rate / 100);  
 }
 
 void SavingsAccount::withdraw(double amount) {
-    if (amount <= 0) {
-        throw std::invalid_argument("El monto del retiro no puede ser negativo.");
+    if (amount < 0) {
+        throw std::invalid_argument("Error: El monto del retiro no puede ser negativo.");
     }
-    if (amount > _balance) {
-        throw std::runtime_error("Fondos insuficientes en la cuenta de ahorros");
+    if (_balance < amount) {
+        throw std::runtime_error("Fondos insuficientes en la cuenta de ahorros.");
     }
-    BankAccount::withdraw(amount);
-}
+    BankAccount::withdraw(amount);  
+
 
 
