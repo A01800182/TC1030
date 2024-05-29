33341e91b16f8a52c02fd15a1d940dc06a6b7977
Miranda Martínez <mirandamartinezjimenez8@gmail.com>
2024-05-29 17:15:31 -0600

    Savings

--- proyecto/SavingsAccount.h
+++ proyecto/SavingsAccount.h
@@ -13,6 +13,11 @@ private:
     double _interest_rate;
 public:
     SavingsAccount(std::string account_number, std::string account_holder_name, double balance, double interest_rate);
+
+    // Sobrescribir método deposit
     void deposit(double amount) override;
+
+    // Sobrescribir método withdraw
     void withdraw(double amount) override;
 };
+
