ed8ce6735aea43e7d436e2d92982cd9bc1f3e1ce
Miranda Martínez <mirandamartinezjimenez8@gmail.com>
2024-05-29 15:06:09 -0600

    Encabezado

---
+++ proyecto/CheckingAccount.cpp
@@ -0,0 +1,7 @@
+// ----------------------------------------------------------
+// Proyecto: Simulador de sistema bancario
+// Fecha: 12 de junio, 2024.
+// Autores:
+//          A01800182 Daniel Alvarez Sil.
+//          A01800935 Miranda Martínez Jiménez.
+// ----------------------------------------------------------
